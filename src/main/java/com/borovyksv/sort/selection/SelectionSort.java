366938c7284cce680d91b061e71a4dd1178e7b89
Sergey <relzet69@gmail.com>
2018-10-22 22:55:44 +0300

    Selection sort implementation

---
+++ src/main/java/com/borovyksv/sort/selection/SelectionSort.java
@@ -0,0 +1,18 @@
+package com.borovyksv.sort.selection;
+
+import com.borovyksv.sort.Sortable;
+
+public class SelectionSort implements Sortable {
+
+    @Override
+    public void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) minIndex = j;
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+}
