017d77a35440142ec56ab6695d00213e4aacf0ef
Sergey <relzet69@gmail.com>
2018-10-23 16:43:25 +0300

    added runner for CountingSort

--- src/main/java/com/borovyksv/sort/counting/CountingSort.java
+++ src/main/java/com/borovyksv/sort/counting/CountingSort.java
@@ -1,6 +1,15 @@
 package com.borovyksv.sort.counting;
 
+import java.util.Arrays;
+
 public class CountingSort {
+    public static void main(String[] args) {
+//        int[] integers = new int[]{5, 1, 5, 4, 3, 2, 1};
+        int[] integers = new int[]{5, 1, 5, 4, 3, 2, 1, 7, 9, 10, 8, 6, 5, 4, 3, 2};
+        sort(integers, 11);
+        System.out.println();
+        System.out.println(Arrays.toString(integers));
+    }
 
     private static void sort(int[] arr, int range) {
         int[] count = new int[range + 1];
